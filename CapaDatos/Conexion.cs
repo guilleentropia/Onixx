421143cb8f27cdda4e15a57e7fca52a0eae406db
guille.rodriguez <guilleentropia>
2016-09-19 13:12:00 -0500

    First Step

---
+++ CapaDatos/Conexion.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaDatos
+{
+    class Conexion
+    {
+        public static string Cn = "Data Source =GLB-D0102 , Initial Catalog= Onixx, Integrated Security = true ";
+    }
+}
